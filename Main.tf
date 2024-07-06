dba414dbfa05ed8d1be0d511db9a5e1340f25355
MackkDevops <iammackk@gmail.com>
2024-07-06 15:19:53 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -2,9 +2,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0dcc1e21636832c5d"
+  ami           = "ami-0862be96e41dcbf74"
   instance_type = "t2.micro"
   tags = {
-      Name = "MyVm"
+      Name = "Docker"
   }
 }
